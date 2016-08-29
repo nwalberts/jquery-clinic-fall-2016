da2120d4a0694c4136e6ab922d5e743dba740d7e
Nick Alberts <nwalberts5@gmail.com>
2016-08-29 16:37:39 -0400

    remove old files

--- app.rb
+++ app.rb
@@ -3,17 +3,3 @@ require 'sinatra'
 get '/' do
   erb :index
 end
-
-get '/items/new' do
-  erb :new
-end
-
-post '/items/new' do
-  @title = params['title']
-  @url = params['url']
-  @description = params['description']
-  CSV.open('foods.csv', 'a') do |csv|
-    csv << [@title, @url, @description]
-  end
-  redirect '/articles'
-end
